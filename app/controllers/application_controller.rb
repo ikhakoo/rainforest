cb57d35ea6155c402e9f25e07bd7a7a64abcee34
ikhakoo <ikhakoo@gmail.com>
2015-04-22 12:18:30 -0400

    cleaning up some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def ensure_logged_in
     unless current_user
       flash[:alert] = "Please log in"
-      redirect_to new_session_path
+      redirect_to root_path
     end
   end
 

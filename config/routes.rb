5027514281ec6bfdddee71f2c5872cfff4050110
ikhakoo <ikhakoo@gmail.com>
2015-04-22 17:21:49 -0400

    fixed a major issue regarding deleting users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews, only: [:show, :create, :destroy]
   end
-  resources :users, only: [:new, :create, :show, :edit, :update]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
 
 end

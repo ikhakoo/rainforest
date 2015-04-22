a81e9678bba1707aca7f1ebde09571765c6d4064
ikhakoo <ikhakoo@gmail.com>
2015-04-22 13:51:11 -0400

    updating design, adding edit function, and forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(product_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
 
 private
   def user_params

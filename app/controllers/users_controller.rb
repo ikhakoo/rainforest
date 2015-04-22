c9803e8d8a641c0391ff243bf67ca212c85c0dde
ikhakoo <ikhakoo@gmail.com>
2015-04-22 16:18:19 -0400

    edit is functional user profile look updated to use css and js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,14 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
 
-    if @user.update_attributes(product_params)
+    if @user.update_attributes(user_params)
       redirect_to user_path(@user)
     else
       render :edit

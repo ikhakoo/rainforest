a7551e5852a6542af199481559a5b2041c19d39f
ikhakoo <ikhakoo@gmail.com>
2015-04-23 00:02:30 -0400

    about me section works now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name, :about_me)
   end
 
 end
